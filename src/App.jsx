f082a131b82dab72a30cca0c1701a187b507444b
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-12-03 21:52:58 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className='test-7xl bg-rose-600 underline'>Vite+Reacts</h1>
-      <h2 className='font-bold text-5xl'>Data</h2>
+      <h2 className='font-bold text-5xl'>Data Plus</h2>
       <h4>Project</h4>
     </>
   )
