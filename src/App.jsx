ab2b14dbb4eb73ffd2b73db29b79a4a3f4fdb580
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-12-16 23:00:40 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <h1 className='test-7xl bg-rose-600 underline'>Vite+Reacts</h1>
       <h2 className='font-bold text-5xl'>Data Plus</h2>
       <h4>Project</h4>
+      
     </>
   )
 }
